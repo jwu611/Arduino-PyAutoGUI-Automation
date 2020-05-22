6386ead9001893827cb56dff9cf53fc04ce4a6a7
jwu611 <65274779+jwu611@users.noreply.github.com>
2020-05-21 17:34:51 -0700

    Update VideoAutomation-PyAutoGUI.py

--- VideoAutomation-PyAutoGUI.py
+++ VideoAutomation-PyAutoGUI.py
@@ -2,8 +2,8 @@ import serial
 import time #Required to use delay functions
 import pyautogui
 
-ArduinoSerial = serial.Serial('com3',9600) #Create Serial port object called arduinoSerialData
-time.sleep(2) #wait for 2 seconds for the communication to get established
+ArduinoSerial = serial.Serial('com3',9600)
+time.sleep(2) #wait for established connection
 
 while 1:
     incoming = str (ArduinoSerial.readline())
